28964ce50ef2e03796e1f3a1ead3c163daad1b8f
Daniel Lorch <dlorch@gmail.com>
2021-01-02 22:59:03 +0200

    Pin terraform provider versions

--- backend.tf
+++ backend.tf
@@ -3,4 +3,15 @@ terraform {
     bucket = "errors-fail-terraform-state"
     prefix = "expired.errors.fail"
   }
+  required_version = "~> 0.13.5"
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 3.51.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0.0"
+    }
+  }
 }
